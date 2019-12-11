923aa5a68e0bae30d2e725aa95996ba3ae19ea0f
Johnstein <joao.neto.feup@gmail.com>
2019-12-11 23:31:38 +0000

    Getting ChannelPackets from data packet

--- RaidillonClient/Raidillon.Client/DataStructure/CarTelemetryPacket.cs
+++ RaidillonClient/Raidillon.Client/DataStructure/CarTelemetryPacket.cs
@@ -2,41 +2,41 @@
 {
     internal class CarTelemetryPacket : DataPacket
     {
-        private class CarTelemetryData
+        internal class CarTelemetryData
         {
-            private ushort m_speed { get; set; }                    // Speed of car in kilometres per hour
+            internal ushort m_speed { get; set; }                    // Speed of car in kilometres per hour
 
-            private float m_throttle { get; set; }                 // Amount of throttle applied (0.0 to 1.0)
+            internal float m_throttle { get; set; }                 // Amount of throttle applied (0.0 to 1.0)
 
-            private float m_steer { get; set; }                    // Steering (-1.0 (full lock left) to 1.0 (full lock right))
+            internal float m_steer { get; set; }                    // Steering (-1.0 (full lock left) to 1.0 (full lock right))
 
-            private float m_brake { get; set; }                    // Amount of brake applied (0.0 to 1.0)
+            internal float m_brake { get; set; }                    // Amount of brake applied (0.0 to 1.0)
 
-            private byte m_clutch { get; set; }                   // Amount of clutch applied (0 to 100)
+            internal byte m_clutch { get; set; }                   // Amount of clutch applied (0 to 100)
 
-            private sbyte m_gear { get; set; }                     // Gear selected (1-8, N=0, R=-1)
+            internal sbyte m_gear { get; set; }                     // Gear selected (1-8, N=0, R=-1)
 
-            private ushort m_engineRPM { get; set; }                // Engine RPM
+            internal ushort m_engineRPM { get; set; }                // Engine RPM
 
-            private byte m_drs { get; set; }                      // 0 = off, 1 = on
+            internal byte m_drs { get; set; }                      // 0 = off, 1 = on
 
-            private byte m_revLightsPercent { get; set; }         // Rev lights indicator (percentage)
+            internal byte m_revLightsPercent { get; set; }         // Rev lights indicator (percentage)
 
-            private ushort[] m_brakesTemperature { get; set; }     // Brakes temperature (celsius)
+            internal ushort[] m_brakesTemperature { get; set; }     // Brakes temperature (celsius)
 
-            private ushort[] m_tyresSurfaceTemperature { get; set; } // Tyres surface temperature (celsius)
+            internal ushort[] m_tyresSurfaceTemperature { get; set; } // Tyres surface temperature (celsius)
 
-            private ushort[] m_tyresInnerTemperature { get; set; } // Tyres inner temperature (celsius)
+            internal ushort[] m_tyresInnerTemperature { get; set; } // Tyres inner temperature (celsius)
 
-            private ushort m_engineTemperature { get; set; }        // Engine temperature (celsius)
+            internal ushort m_engineTemperature { get; set; }        // Engine temperature (celsius)
 
-            private float[] m_tyresPressure { get; set; }         // Tyres pressure (PSI)
+            internal float[] m_tyresPressure { get; set; }         // Tyres pressure (PSI)
 
-            private byte[] m_surfaceType { get; set; }           // Driving surface, see appendices 
+            internal byte[] m_surfaceType { get; set; }           // Driving surface, see appendices 
         }
 
-        private CarTelemetryData[] m_carTelemetryData { get; set; }
+        internal CarTelemetryData[] m_carTelemetryData { get; set; }
 
-        private uint m_buttonStatus { get; set; }
+        internal uint m_buttonStatus { get; set; }
     }
 }
